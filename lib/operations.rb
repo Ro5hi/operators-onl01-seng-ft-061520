95a2fc6b166354dd11072d923088383187f56a61
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-16 21:57:32 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,12 @@
-def unsafe("79")
-
-  79 > 60 ? ("true") : ("false")
+def unsafe?(speed)
+  
+  if speed > 60 || speed < 40
+    true
+  else 
+    false
+  end
 end
 
 def not_safe?(speed)
-  
-end
-	
-
-
+    speed < 40 || speed > 60 ? true : false	
+  end
