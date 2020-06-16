7d4c5143838e60eb5f57c8ca6b784517bceeb4ef
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-16 21:41:54 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,11 +2,11 @@ def unsafe?(speed)
   
   if speed > 60 || speed < 40
     true
-  if speed == 40
-    false
+      if speed == 40
+        false
+    end
   end
 end
-
 # def not_safe?(speed)
   
 # end
